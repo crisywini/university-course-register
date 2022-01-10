5651aca8d4f36a6c9fda57c776d616140c5b718e
Cristian Sánchez <christian.sanchez@kinesso.com>
2022-01-09 22:31:14 -0500

    Added findAll course integration test

--- university-data/src/main/java/co/perficient/university/service/course/FindAllCoursesService.java
+++ university-data/src/main/java/co/perficient/university/service/course/FindAllCoursesService.java
@@ -3,19 +3,16 @@ package co.perficient.university.service.course;
 
 import co.perficient.university.model.dto.CourseDto;
 import co.perficient.university.port.CourseRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@RequiredArgsConstructor
 @Service
 public class FindAllCoursesService {
 
     private final CourseRepository courseRepository;
-
-    public FindAllCoursesService(CourseRepository courseRepository) {
-        this.courseRepository = courseRepository;
-    }
-
 
     public Set<CourseDto> findAll() {
         return courseRepository.findAll();
---
+++ university-data/src/test/java/co/perficient/university/service/course/FindAllCoursesServiceTest.java
@@ -0,0 +1,47 @@
+package co.perficient.university.service.course;
+
+import co.perficient.university.model.dto.CourseDto;
+import co.perficient.university.port.CourseRepository;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class FindAllCoursesServiceTest {
+
+    @Mock
+    private CourseRepository courseRepository;
+
+    private FindAllCoursesService findAllCoursesService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        findAllCoursesService = new FindAllCoursesService(courseRepository);
+    }
+
+    @Test
+    void findAll_shouldCallAService() {
+        // Given
+        given(courseRepository.findAll()).willReturn(new HashSet<>());
+        // When
+        Set<CourseDto> all = findAllCoursesService.findAll();
+        // Then
+        Assertions.assertEquals(0, all.size());
+        verify(courseRepository, times(1)).findAll();
+    }
+
+}
